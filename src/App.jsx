045506eeadff1e196147da9b2651cacd57d7409d
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-03-25 22:27:08 -0700

    city lat + long working

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import FormSearch from './FormSearch.jsx'
+
+import axios from 'axios';
+
+//Note to self: const varibles must be kept capitalized to avoid confusion with variables.
+const LOCATION_IQ_API_KEY = import.meta.env.LOCATION_IQ;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [responseData, setResponseData] = useState({});
+  const [error, setError] = useState(null);
+  const [city, setCity] = useState('');
+
+  const handleInput = (event) => {
+    let value = event.target.value;
+    setCity(value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getLocation(city);
+  }
+
+  const getLocation = async (city) => {
+    let response = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${LOCATION_IQ_API_KEY}&q=${city}&format=json`);
+    console.log(response);
+    setResponseData(response.data[0]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <header>
+        <form>
+          <input type="text" placeholder="Enter a city name" onChange={handleInput} />
+          <button onClick={handleSubmit}>
+            Search
+          </button>
+        </form>
+      </header>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {responseData.display_name
+        ? <ol>
+            <p>{responseData.display_name}</p>
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${LOCATION_IQ_API_KEY}&center=${responseData.lat},${responseData.lon}&zoom=9`}/>
+          </ol>
+        : <p>Please Click the button</p>
+        }
+        <button onClick={() => handleNext(responseData?.next)}>Next</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
